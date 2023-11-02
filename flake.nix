d78b2a2cdc54316382b5b654d6782eae93d4f5e6
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-11-02 09:53:38 -0400

    ok

--- flake.nix
+++ flake.nix
@@ -25,30 +25,66 @@
           pkgs = import nixpkgs {
             inherit system overlays;
           };
+        maxMindKey = builtins.readFile ./maxmind_license.txt;
+        MAXMIND_BASE_URL="https://download.maxmind.com/app/geoip_download?license_key=${maxMindKey}";
         buildInputs = with pkgs; [ rust-bin.stable.latest.default flyctl nodejs ];
         craneLib = crane.lib.${system};
         src = craneLib.cleanCargoSource (craneLib.path ./backend);
         commonArgs = {
             inherit src buildInputs;
           };
-        # cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
         bin = craneLib.buildPackage (commonArgs // {
-          # inherit cargoArtifacts;
+          inherit cargoArtifacts;
           cargoLock = ./backend/Cargo.lock;
           cargoToml = ./backend/Cargo.toml;
-          postUnpack = ''
-            cd $sourceRoot/backend
-            sourceRoot="."
-          '';
           });
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "tools-backend";
+          tag = "latest";
+          copyToRoot = [bin];
+          runAsRoot = ''
+              mkdir /GeoLite2/
+              cd /GeoLite2/
+              URL="${MAXMIND_BASE_URL}"
+              URL="$URL&edition_id=GeoLite2-ASN&suffix=tar.gz"
+              wget "$URL" -O GeoLite2-ASN.tar.gz
+
+              URL="${MAXMIND_BASE_URL}"
+              URL="$URL&edition_id=GeoLite2-ASN&suffix=tar.gz.sha256"
+              wget "$URL" -O GeoLite2-ASN.tar.gz.sha256
+
+              URL="${MAXMIND_BASE_URL}"
+              URL="$URL&edition_id=GeoLite2-City&suffix=tar.gz"
+              wget "$URL" -O GeoLite2-City.tar.gz
+
+              URL="${MAXMIND_BASE_URL}"
+              URL="$URL&edition_id=GeoLite2-City&suffix=tar.gz.sha256"
+              wget "$URL" -O GeoLite2-City.tar.gz.sha256
+
+              sed 's/GeoLite2-ASN_[0-9]*.tar.gz/GeoLite2-ASN.tar.gz/g' -i GeoLite2-ASN.tar.gz.sha256
+              sha256sum -c GeoLite2-ASN.tar.gz.sha256
+              tar xvf GeoLite2-ASN.tar.gz --strip 1
+
+              sed 's/GeoLite2-City_[0-9]*.tar.gz/GeoLite2-City.tar.gz/g' -i GeoLite2-City.tar.gz.sha256
+              sha256sum -c GeoLite2-City.tar.gz.sha256
+              tar xvf GeoLite2-City.tar.gz --strip 1
+              mkdir -p /data/GeoIP
+              mv /GeoLite2/*.mmdb /data/GeoIP
+              '';
+          config = {
+            Cmd = [ "${bin}/bin/tool-api" ];
+          };
+        };
         in with pkgs;
         {
           packages  = {
-              inherit bin;
+              inherit bin dockerImage;
               default = bin;
           };
           devShells.default = mkShell {
             inputsFrom = [bin];
+            buildInputs = [pkgs.dive pkgs.git-crypt];
           };
         }
       );
